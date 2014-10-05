4c179400c876733d9debd7ca4afb35d600b3e574
Michael Flynn <mflynn37211@yahoo.com>
2014-10-04 22:50:13 -0400

    Added Manufacturers tests Added Manufacturers factories Added Manufacturers models Added Manufacturers controller all test passing

---
+++ spec/factories/manufacturers.rb
@@ -0,0 +1,6 @@
+FactoryGirl.define do
+  factory :manufacturer do
+    name 'BMW'
+    country 'Germany'
+  end
+end
---
+++ spec/features/user_creates_a_new_manufacturer_spec.rb
@@ -0,0 +1,22 @@
+require 'rails_helper'
+
+feature 'user adds a new manufacturer' do
+
+  scenario 'user can add a manufacturer' do
+    manufacturer = FactoryGirl.build(:manufacturer)
+
+    visit new_manufacturer_path
+    fill_in 'Name', with: manufacturer.name
+    fill_in 'Country', with: manufacturer.country
+    click_on 'Add Manufacturer'
+
+    expect(page).to have_content manufacturer.name
+  end
+
+  scenario 'user tries to add a blank manufacturer' do
+    visit new_manufacturer_path
+
+    click_on 'Add Manufacturer'
+    expect(page).to have_content "We couldn\'t save your manufacturer, please insert information in both fields!"
+  end
+end
