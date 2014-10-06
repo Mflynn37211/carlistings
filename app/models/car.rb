c52e4e343f8c40870f90a7b88071ce266c916880
Helen Hood <helen.c.hood@gmail.com>
2014-10-06 17:28:31 -0400

    Refactor validations

--- app/models/car.rb
+++ app/models/car.rb
@@ -2,8 +2,8 @@ class Car < ActiveRecord::Base
   belongs_to :manufacturer
 
   validates :model_name, presence: true
-  validates :year, presence: true, numericality: { greater_than: 1919, less_than: 2015 }
-  validates :mileage, presence: true, numericality: { greater_than: -1 }
+  validates :year, presence: true, numericality: { greater_than: 1919, less_than_or_equal_to: Time.now.year }
+  validates :mileage, presence: true, numericality: { greater_than_or_equal_to: 0 }
   validates :color, presence: true
-  validates :manufacturer_id, presence: true
+  validates :manufacturer, presence: true
 end
